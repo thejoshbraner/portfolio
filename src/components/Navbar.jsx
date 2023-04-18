0a1d7676826c1056508a8db169b3201c3a6bf6b1
Josh Braner <braner.josh@gmail.com>
2023-04-18 08:51:16 -0500

    fixed tailwind, was not installed correctly

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="w-full h-10 bg-slate-600">
+            <a href="/">Home</a>
+            <a href="/">About</a>
+            <a href="/">Contact</a>
+        </div>
+    );
+};
+
+export default Navbar;

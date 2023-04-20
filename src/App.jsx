ce5e5eaabd71a4f1b8829ba568a25c826a8890ab
Josh Braner <braner.josh@gmail.com>
2023-04-20 09:01:05 -0500

    changed font again, added icon... still not sure what I want to make but progress is progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import icon from "./assets/pixelicon.jpg";
 
 //component imports
 import Navbar from "./components/navbar";
@@ -7,8 +8,13 @@ function App() {
     return (
         <>
             <Navbar />
-            <div className="flex justify-center content-center">
+            <div className="flex flex-col justify-center items-center content-center">
+                <img className="mbç-4 rounded-full h-40 w-40" src={icon} />
                 <h1>This is the beginning. And boy is it ugly.</h1>
+                <p className="text-sm px-40 my-2 text-center">
+                    this font probably isn't going to work... reminder to import and try "kongtext" also what the heck
+                    are you going to do about design dude? what's your layout? go wireframe something ya dingy
+                </p>
             </div>
         </>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <div className="w-full h-10 text-rose-600 bg-white flex flex-row gap-x-2 p-2 justify-center">
+        <div className="w-full h-10 text-rose-600 bg-white flex flex-row gap-x-2 p-2 m-3 justify-center">
             <a href="/">Home</a>
             <a href="/">About</a>
             <a href="/">Contact</a>
